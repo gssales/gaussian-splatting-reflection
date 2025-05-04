98fb941a7cf87fa2a52460e3c7b14620af1f43b6
Guilherme Sales <guilherme.sales@inf.ufrgs.br>
2025-05-04 19:00:58 -0300

    format rasterizar files

--- submodules/diff-gaussian-rasterization/rasterize_points.h
+++ submodules/diff-gaussian-rasterization/rasterize_points.h
@@ -3,7 +3,7 @@
  * GRAPHDECO research group, https://team.inria.fr/graphdeco
  * All rights reserved.
  *
- * This software is free for non-commercial, research and evaluation use 
+ * This software is free for non-commercial, research and evaluation use
  * under the terms of the LICENSE.md file.
  *
  * For inquiries contact  george.drettakis@inria.fr
@@ -14,65 +14,65 @@
 #include <cstdio>
 #include <tuple>
 #include <string>
-	
+
 //         rendered, out_color, radii,        geomBuffer,    binningBuffer, imgBuffer,     out_invdepth,  normal_map,    refl_strength_map
 std::tuple<int, torch::Tensor, torch::Tensor, torch::Tensor, torch::Tensor, torch::Tensor, torch::Tensor, torch::Tensor, torch::Tensor>
 RasterizeGaussiansCUDA(
-	const torch::Tensor& background,
-	const torch::Tensor& means3D,
-    const torch::Tensor& colors,
-	const torch::Tensor& normals,
-	const torch::Tensor& refl_strengths,
-    const torch::Tensor& opacity,
-	const torch::Tensor& scales,
-	const torch::Tensor& rotations,
-	const float scale_modifier,
-	const torch::Tensor& cov3D_precomp,
-	const torch::Tensor& viewmatrix,
-	const torch::Tensor& projmatrix,
-	const float tan_fovx, 
-	const float tan_fovy,
-    const int image_height,
-    const int image_width,
-	const torch::Tensor& sh,
-	const int degree,
-	const torch::Tensor& campos,
-	const bool prefiltered,
-	const bool antialiasing,
-	const bool debug);
+		const torch::Tensor &background,
+		const torch::Tensor &means3D,
+		const torch::Tensor &colors,
+		const torch::Tensor &normals,
+		const torch::Tensor &refl_strengths,
+		const torch::Tensor &opacity,
+		const torch::Tensor &scales,
+		const torch::Tensor &rotations,
+		const float scale_modifier,
+		const torch::Tensor &cov3D_precomp,
+		const torch::Tensor &viewmatrix,
+		const torch::Tensor &projmatrix,
+		const float tan_fovx,
+		const float tan_fovy,
+		const int image_height,
+		const int image_width,
+		const torch::Tensor &sh,
+		const int degree,
+		const torch::Tensor &campos,
+		const bool prefiltered,
+		const bool antialiasing,
+		const bool debug);
 
 std::tuple<torch::Tensor, torch::Tensor, torch::Tensor, torch::Tensor, torch::Tensor, torch::Tensor, torch::Tensor, torch::Tensor, torch::Tensor, torch::Tensor>
- RasterizeGaussiansBackwardCUDA(
- 	const torch::Tensor& background,
-	const torch::Tensor& means3D,
-	const torch::Tensor& radii,
-    const torch::Tensor& colors,
-	const torch::Tensor& normals,
-	const torch::Tensor& refl_strengths,
-	const torch::Tensor& opacities,
-	const torch::Tensor& scales,
-	const torch::Tensor& rotations,
-	const float scale_modifier,
-	const torch::Tensor& cov3D_precomp,
-	const torch::Tensor& viewmatrix,
-    const torch::Tensor& projmatrix,
-	const float tan_fovx, 
-	const float tan_fovy,
-    const torch::Tensor& dL_dout_color,
-	const torch::Tensor& dL_dout_invdepth,
-	const torch::Tensor& dL_dout_normal_map,
-	const torch::Tensor& dL_dout_refl_strength_map,
-	const torch::Tensor& sh,
-	const int degree,
-	const torch::Tensor& campos,
-	const torch::Tensor& geomBuffer,
-	const int R,
-	const torch::Tensor& binningBuffer,
-	const torch::Tensor& imageBuffer,
-	const bool antialiasing,
-	const bool debug);
-		
+RasterizeGaussiansBackwardCUDA(
+		const torch::Tensor &background,
+		const torch::Tensor &means3D,
+		const torch::Tensor &radii,
+		const torch::Tensor &colors,
+		const torch::Tensor &normals,
+		const torch::Tensor &refl_strengths,
+		const torch::Tensor &opacities,
+		const torch::Tensor &scales,
+		const torch::Tensor &rotations,
+		const float scale_modifier,
+		const torch::Tensor &cov3D_precomp,
+		const torch::Tensor &viewmatrix,
+		const torch::Tensor &projmatrix,
+		const float tan_fovx,
+		const float tan_fovy,
+		const torch::Tensor &dL_dout_color,
+		const torch::Tensor &dL_dout_invdepth,
+		const torch::Tensor &dL_dout_normal_map,
+		const torch::Tensor &dL_dout_refl_strength_map,
+		const torch::Tensor &sh,
+		const int degree,
+		const torch::Tensor &campos,
+		const torch::Tensor &geomBuffer,
+		const int R,
+		const torch::Tensor &binningBuffer,
+		const torch::Tensor &imageBuffer,
+		const bool antialiasing,
+		const bool debug);
+
 torch::Tensor markVisible(
-		torch::Tensor& means3D,
-		torch::Tensor& viewmatrix,
-		torch::Tensor& projmatrix);
+		torch::Tensor &means3D,
+		torch::Tensor &viewmatrix,
+		torch::Tensor &projmatrix);
