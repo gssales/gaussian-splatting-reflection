9de19596f57c1d8ad743ca72a53295be64c35fde
Guilherme Sales <guilherme.sales@inf.ufrgs.br>
2025-06-23 22:19:52 -0300

    update eval

--- full_eval.py
+++ full_eval.py
@@ -13,32 +13,44 @@ import os
 from argparse import ArgumentParser
 import time
 
-# ref_real_scenes = ["ref_real/sedan", "ref_real/gardenspheres", "ref_real/toycar"]
-# refnerf_scenes = ["shiny_blender/helmet","shiny_blender/car","shiny_blender/ball","shiny_blender/teapot","shiny_blender/coffee","shiny_blender/toaster"]
-# nerf_synthetic_scenes = ["nerf_synthetic/ship","nerf_synthetic/ficus","nerf_synthetic/lego","nerf_synthetic/mic","nerf_synthetic/hotdog","nerf_synthetic/chair","nerf_synthetic/materials","nerf_synthetic/drums"]
-# glossy_synthetic_scenes = ["GlossySynthetic/bell","GlossySynthetic/tbell","GlossySynthetic/potion","GlossySynthetic/teapot","GlossySynthetic/luyu","GlossySynthetic/cat"]
-
-ref_real_scenes = []
-refnerf_scenes = ["shiny_blender/helmet","shiny_blender/ball","shiny_blender/coffee"]
-nerf_synthetic_scenes = []
-glossy_synthetic_scenes = ["GlossySynthetic/tbell","GlossySynthetic/potion"]
+ref_real_scenes = ["ref_real/gardenspheres", "ref_real/sedan", "ref_real/toycar"]
+refnerf_scenes = ["shiny_blender/ball","shiny_blender/car","shiny_blender/coffee","shiny_blender/helmet","shiny_blender/teapot","shiny_blender/toaster"]
+nerf_synthetic_scenes = ["nerf_synthetic/chair","nerf_synthetic/drums","nerf_synthetic/ficus","nerf_synthetic/hotdog","nerf_synthetic/lego","nerf_synthetic/materials","nerf_synthetic/mic","nerf_synthetic/ship"]
+glossy_synthetic_scenes = ["GlossySynthetic/angel","GlossySynthetic/bell","GlossySynthetic/cat","GlossySynthetic/horse","GlossySynthetic/luyu","GlossySynthetic/potion","GlossySynthetic/tbell","GlossySynthetic/teapot"]
 
 
 parser = ArgumentParser(description="Full evaluation script parameters")
 parser.add_argument("--skip_training", action="store_true")
 parser.add_argument("--skip_rendering", action="store_true")
 parser.add_argument("--skip_metrics", action="store_true")
-parser.add_argument("--output_path", default="./eval")
+parser.add_argument("--output_path", default="/mnt/output/ours/eval")
 
 extra_args = {
     "ref_real/sedan": " -r 8 --env_scope_center -0.032 0.808 0.751 --env_scope_radius 2.138",
     "ref_real/gardenspheres": " -r 4 --env_scope_center -0.2270 1.9700 1.7740 --env_scope_radius 0.974",
     "ref_real/toycar": " -r 4 --env_scope_center 0.486 1.108 3.72 --env_scope_radius 2.507",
-    "shiny_blender/helmet": " --init_until_iter 0",
     "shiny_blender/ball": " -w --init_until_iter 0 --sythetic",
-    "shiny_blender/coffee": " --init_until_iter 3000 --synthetic",
+    "shiny_blender/car": " --opacity_reset_interval 1000 --synthetic",
+    "shiny_blender/coffee": " --init_until_iter 3000 --synthetic  --densification_interval_when_prop 500",
+    "shiny_blender/helmet": " --init_until_iter 0",
+    "shiny_blender/teapot": " --opacity_reset_interval 1000 --synthetic",
+    "shiny_blender/toaster": " --opacity_reset_interval 1000 --synthetic",
+    "GlossySynthetic/angel": " --init_until_iter 20 --synthetic --densification_interval_when_prop 500",
+    "GlossySynthetic/bell": " --opacity_reset_interval 1000 --synthetic",
+    "GlossySynthetic/cat": " --opacity_reset_interval 1000 --synthetic",
+    "GlossySynthetic/horse": " --init_until_iter 20 --synthetic --densification_interval_when_prop 500",
+    "GlossySynthetic/luyu": " --opacity_reset_interval 1000 --synthetic",
+    "GlossySynthetic/potion": " -w --init_until_iter 20 --sythetic",
     "GlossySynthetic/tbell": " -w --init_until_iter 20 --sythetic",
-    "GlossySynthetic/potion": " -w --init_until_iter 20 --sythetic"
+    "GlossySynthetic/teapot": " --opacity_reset_interval 1000 --synthetic",
+    "nerf_synthetic/chair": " --opacity_reset_interval 1000 --synthetic",
+    "nerf_synthetic/drums": " --opacity_reset_interval 1000 --synthetic",
+    "nerf_synthetic/ficus": " --opacity_reset_interval 1000 --synthetic",
+    "nerf_synthetic/hotdog": " --opacity_reset_interval 1000 --synthetic",
+    "nerf_synthetic/lego": " --opacity_reset_interval 1000 --synthetic",
+    "nerf_synthetic/materias": " --opacity_reset_interval 1000 --synthetic",
+    "nerf_synthetic/mic": " --opacity_reset_interval 1000 --synthetic",
+    "nerf_synthetic/ship": " --opacity_reset_interval 1000 --synthetic"
 }
 
 
@@ -63,7 +75,7 @@ if not args.skip_training:
     for scene in ref_real_scenes:
         source = args.ref_real + "/" + scene
         extra = extra_args[scene]
-        more_args = " --init_until_iter 3000 --lambda_dist 100 --opacity_reset_interval 1000 --use_env_scope"
+        more_args = " --iterations 60000 --init_until_iter 3000 --lambda_dist 100 --opacity_reset_interval 1000 --use_env_scope"
         os.system("python train.py -s " + source + " -m " + args.output_path + "/" + scene + common_args + extra + more_args)
     ref_real_timing = (time.time() - start_time)/60.0
     
@@ -107,6 +119,7 @@ if not args.skip_rendering:
     for scene, source in zip(all_scenes, all_sources):
         # os.system("python render.py --iteration 7000 -s " + source + " -m " + args.output_path + "/" + scene + common_args)
         os.system("python render.py --iteration 30000 -s " + source + " -m " + args.output_path + "/" + scene + common_args)
+        os.system("python render.py --iteration 60000 -s " + source + " -m " + args.output_path + "/" + scene + common_args)
 
 if not args.skip_metrics:
     scenes_string = ""
